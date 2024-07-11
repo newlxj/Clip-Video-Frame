c13a7c88537bff352c1285cf88f9017a17826205
aliu <inlhx@163.com>
2024-07-11 22:47:04 +0800

    fix build

--- buildcn.bat
+++ buildcn.bat
@@ -4,27 +4,27 @@ SET CGO_ENABLED=0
 SET GOARCH=amd64
 
 SET GOOS=linux
-go build -ldflags "-w -s" -o build\ClipVideo-linux-x64
+go build -ldflags "-w -s" -o build\ClipVideoFrame-linux-x64
 
 SET GOOS=windows
-go build  -ldflags "-w -s" -o build\ClipVideo.exe
+go build  -ldflags "-w -s" -o build\ClipVideoFrame.exe
 
 
 SET GOOS=darwin
-go build -ldflags "-w -s" -o build\ClipVideo-macos-x64
+go build -ldflags "-w -s" -o build\ClipVideoFrame-macos-x64
 
 SET GOARCH=386
 SET GOOS=windows
-go build  -ldflags "-w -s" -o build\ClipVideo-x86.exe
+go build  -ldflags "-w -s" -o build\ClipVideoFrame-x86.exe
 
 
 SET GOOS=linux
-go build -ldflags "-w -s" -o build\ClipVideo-linux86
+go build -ldflags "-w -s" -o build\ClipVideoFrame-linux86
 
 SET GOOS=linux
 SET GOARCH=arm
-go build -ldflags "-w -s" -o build\ClipVideo-linux-arm
+go build -ldflags "-w -s" -o build\ClipVideoFrame-linux-arm
 
 
 SET GOOS=darwin
-go build -ldflags "-w -s" -o build\ClipVideo-macos-arm
+go build -ldflags "-w -s" -o build\ClipVideoFrame-macos-arm
--- builden.bat
+++ builden.bat
@@ -5,29 +5,29 @@ SET CGO_ENABLED=0
 SET GOARCH=amd64
 
 SET GOOS=linux
-go build -ldflags "-w -s" -o build-en\ClipVideo-linux-x64
+go build -ldflags "-w -s" -o build-en\ClipVideoFrame-linux-x64
 
 SET GOOS=windows
-go build  -ldflags "-w -s" -o build-en\ClipVideo.exe
+go build  -ldflags "-w -s" -o build-en\ClipVideoFrame.exe
 
 SET GOOS=darwin
-go build -ldflags "-w -s" -o build-en\ClipVideo-macos-x64
+go build -ldflags "-w -s" -o build-en\ClipVideoFrame-macos-x64
 
 SET GOARCH=386
 SET GOOS=windows
-go build  -ldflags "-w -s" -o build-en\ClipVideo-x86.exe
+go build  -ldflags "-w -s" -o build-en\ClipVideoFrame-x86.exe
 
 
 SET GOOS=linux
-go build -ldflags "-w -s" -o build-en\ClipVideo-linux86
+go build -ldflags "-w -s" -o build-en\ClipVideoFrame-linux86
 
 SET GOOS=linux
 SET GOARCH=arm
-go build -ldflags "-w -s" -o build-en\ClipVideo-linux-arm
+go build -ldflags "-w -s" -o build-en\ClipVideoFrame-linux-arm
 
 
 SET GOOS=darwin
-go build -ldflags "-w -s" -o build-en\ClipVideo-macos-arm
+go build -ldflags "-w -s" -o build-en\ClipVideoFrame-macos-arm
 
 
 rename main.go main.go.en
